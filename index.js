19d1b120bee55fa57d1b21ba8760403d06fcc9a1
Rupam Basak <rups20097@gmail.com>
2023-06-26 23:56:44 +0530

    integrated with api call

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $('#submitButt').click(function () {
 });
 $(function () {
     $("#test").focus();
+    sessionStorage.setItem('sessionId', Math.floor(Math.random() * 37 ))
 });
 document.querySelector('.floating-btn').addEventListener('click', function (e) {
     e.target.closest('button').classList.toggle('clicked');
@@ -23,10 +24,41 @@ function myFunction() {
 
 function send_text() {
     if ($('#text_typed').val() !== '') {
+        let query = $('#text_typed').val();
         $('#chatroom_box').append(`
-            <li class="user_text">${$('#text_typed').val()}</li>
+            <li class="user_text">${query}</li>
         `)
         $('#text_typed').val('');
+
+        let data = {
+            "queryText": query,
+            "sessionId": sessionStorage.getItem('sessionId')
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/test",
+            data: JSON.stringify(data),// now data come in this function
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+            },
+            dataType: "json",
+            success: function (data, status, jqXHR) {
+                $('#chatroom_box').append(`
+                    <li class="bot_text">${data.text}</li>
+                `)
+            },
+
+            error: function (jqXHR, status) {
+                // error handler
+                console.log(jqXHR);
+                alert('fail' + status.code);
+            }
+         });
+
     }
 }
 
